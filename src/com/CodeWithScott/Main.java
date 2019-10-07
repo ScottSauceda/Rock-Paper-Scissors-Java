aa6af72c90e584a1f1866d3cf21b73d776f3c758
Scott Sauceda <msauceda0812@gmail.com>
2019-10-07 14:05:48 -0400

    first bonus branch commit

--- src/com/CodeWithScott/Main.java
+++ src/com/CodeWithScott/Main.java
@@ -1,5 +1,7 @@
 package com.CodeWithScott;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -30,7 +32,10 @@ public class Main {
 
 
 
+
     public static void main(String[] args) {
+
+        System.out.println("Bonus Branch");
 
         System.out.println("Welcome to Rock, Paper, Scissors \n");
 
