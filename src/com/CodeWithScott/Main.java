a5ed066923d73e69e30a56fd213ce51b6f6efda4
Scott Sauceda <msauceda0812@gmail.com>
2019-10-07 14:08:45 -0400

    master branch commit

--- src/com/CodeWithScott/Main.java
+++ src/com/CodeWithScott/Main.java
@@ -31,6 +31,7 @@ public class Main {
 
 
     public static void main(String[] args) {
+        System.out.println("Master Branch");
 
         System.out.println("Welcome to Rock, Paper, Scissors \n");
 
